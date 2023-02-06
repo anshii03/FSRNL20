378b7d63240d9ccfe5051147f0364ef4b00ebcc1
Anshika Agarwal <anshika@Anshikas-MacBook-Pro.local>
2023-02-06 10:22:40 +0530

    Understanding react components and props

---
+++ src/Components/User/User.js
@@ -0,0 +1,29 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import "./User.css"
+
+function User(props) {
+
+    // Object destructuring
+    const {details} = props;
+
+    return (
+        <div className="userCard">
+            <Card>
+                <div className="userContent">
+                <Card.Img variant="top" src={details.image} className="img" />
+                <Card.Body>
+                    <Card.Title>{details.title}</Card.Title>
+                    <Card.Text className="description">
+                        {details.description}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                </div>    
+            </Card>    
+        </div>
+          
+    )
+}
+
+export default User
